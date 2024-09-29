cdbf4553c7a191bfc20eb06874154c37f9a211d0
Masum Billal <proafxin@users.noreply.github.com>
2024-09-29 07:07:48 +0600

    added read query and docker compose

---
+++ tests/conftest.py
@@ -0,0 +1,53 @@
+import os
+from typing import AsyncGenerator
+
+import pytest_asyncio
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from xin.db import SQLDatabaseDialect, async_sql_engine, sync_sql_engine
+
+
+@pytest_asyncio.fixture(scope="session")
+async def mysql_engine() -> AsyncGenerator[AsyncEngine, None]:
+    engine = await async_sql_engine(
+        user=os.environ["MYSQL_USER"],
+        password=os.environ["MYSQL_PASSWORD"],
+        port=int(os.environ["MYSQL_PORT"]),
+        dialect=SQLDatabaseDialect.MYSQL,
+        host="localhost",
+        dbname="xin",
+    )
+
+    yield engine
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def postgres_engine() -> AsyncGenerator[AsyncEngine, None]:
+    engine = await async_sql_engine(
+        user=os.environ["POSTGRESQL_USER"],
+        password=os.environ["POSTGRESQL_PASSWORD"],
+        port=int(os.environ["POSTGRESQL_PORT"]),
+        dialect=SQLDatabaseDialect.POSTGRESQL,
+        host="localhost",
+        dbname="xin",
+    )
+
+    yield engine
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def sqlserver_engine() -> AsyncGenerator[Engine, None]:
+    engine = sync_sql_engine(
+        user=os.environ["SQLSERVER_USER"],
+        password=os.environ["SQLSERVER_PASSWORD"],
+        port=int(os.environ["SQLSERVER_PORT"]),
+        dialect=SQLDatabaseDialect.SQLSERVER,
+        host="localhost",
+        dbname="master",
+    )
+
+    yield engine
+    engine.dispose()
--- xin/db.py
+++ xin/db.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+from sqlalchemy.engine import Engine, create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
 
 class SQLDatabaseDialect(str, Enum):
     """SQL database flavors."""
@@ -32,3 +35,28 @@ SQL_DRIVERS = {
 }
 
 NOSQL_DRIVERS = {NoSQLDatabaseDialect.MONGODB.value: NoSQLDatabaseDriver.MOTOR.value}
+
+
+def create_connection_string(
+    user: str, password: str, port: int, dialect: SQLDatabaseDialect, host: str, dbname: str
+) -> str:
+    dbinfo = dialect.value
+    if dbinfo in SQL_DRIVERS:
+        dbinfo = f"{dbinfo}+{SQL_DRIVERS[dialect]}"
+
+    connection_string = f"{dbinfo}://{user}:{password}@{host}:{port}/{dbname}"
+
+    return connection_string
+
+
+async def async_sql_engine(
+    user: str, password: str, port: int, dialect: SQLDatabaseDialect, host: str, dbname: str
+) -> AsyncEngine:
+    url = create_connection_string(user=user, password=password, port=port, dialect=dialect, host=host, dbname=dbname)
+    return create_async_engine(url=url)
+
+
+def sync_sql_engine(user: str, password: str, port: int, dialect: SQLDatabaseDialect, host: str, dbname: str) -> Engine:
+    url = create_connection_string(user=user, password=password, port=port, dialect=dialect, host=host, dbname=dbname)
+
+    return create_engine(url=url)
