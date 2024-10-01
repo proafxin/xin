55191da6ea868dd4ffef442860ec5899783d0f9e
Masum Billal <proafxin@users.noreply.github.com>
2024-10-01 07:21:10 +0600

    added list and dict type check

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ import pytest
 from pandas import DataFrame
 from pydantic import BaseModel
 
-from xin.parser import Base, serialize_table_pandas
+from xin.parser import Base, serialize_table
 
 
 @pytest.mark.asyncio
@@ -14,12 +14,18 @@ async def test_base() -> None:
 
 @pytest.mark.asyncio
 async def test_serialize_pandas_table() -> None:
-    data1 = {"name": "xin", "id": 200100, "email": "anemail@email.com", "created_at": datetime.now(), "balance": 200.0}
-    data2 = {"name": "xin", "id": 200100, "email": "someemail@email.com", "created_at": datetime.now(), "balance": None}
+    now = datetime.now()
+    data1 = {"n": "xin", "id": 200, "f": ["a", "b", "c"], "c": now, "b": 20.0, "d": {"a": 1}}
+    data2 = {"n": "xin", "id": 200, "f": ["d", "e", "f"], "c": now, "b": None, "d": {"a": 1}}
 
-    pydantic_objects = await serialize_table_pandas(table_name="some_table", data=DataFrame(data=[data1, data2]))
+    pydantic_objects = await serialize_table(table_name="some_table", data=DataFrame(data=[data1, data2]))
 
     assert isinstance(pydantic_objects, list)
     for entry in pydantic_objects:
         assert isinstance(entry, BaseModel)
         assert len(entry.model_fields.keys()) >= 5
+
+
+@pytest.mark.asyncio
+async def test_serialize_polars_table() -> None:
+    pass
--- xin/parser.py
+++ xin/parser.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from enum import Enum, auto
 from typing import Any, Type
 
 import numpy as np
 import pandas as pd
+import polars as pl
 from pydantic import BaseModel, TypeAdapter, create_model
 
 
@@ -14,13 +16,18 @@ class QueryResult(Base):
     table_name: str
 
 
-async def serialize_table_pandas(table_name: str, data: pd.DataFrame) -> list[BaseModel]:
+class DataFrameBackend(Enum):
+    PANDAS = auto()
+    POLARS = auto()
+
+
+async def _serialize_table_pandas(table_name: str, data: pd.DataFrame) -> list[BaseModel]:
     dtypes = data.dtypes
     fields: dict[str, Any] = {}
 
     columns = data.columns.tolist()
     dtypes = data.dtypes
-    field_type: Type[int | str | float | datetime] | None
+    field_type: Type[int | str | float | datetime | dict[Any, Any] | list[Any]] | None
 
     for dtype, column in zip(dtypes, columns):
         if np.issubdtype(dtype, np.integer):
@@ -30,7 +37,13 @@ async def serialize_table_pandas(table_name: str, data: pd.DataFrame) -> list[Ba
         elif np.issubdtype(dtype, np.datetime64):
             field_type = datetime
         else:
-            field_type = str
+            value = data[column].iloc[0]
+            if isinstance(value, dict):
+                field_type = dict[Any, Any]
+            elif isinstance(value, list):
+                field_type = list[Any]
+            else:
+                field_type = str
 
         if data[column].dropna().shape[0] < data.shape[0]:
             fields[column] = (field_type | None, ...)
@@ -41,3 +54,16 @@ async def serialize_table_pandas(table_name: str, data: pd.DataFrame) -> list[Ba
     ta = TypeAdapter(list[Model])  # type: ignore[valid-type]
 
     return ta.validate_python(data.to_dict("records"))
+
+
+async def _serialize_table_polars(table_name: str, data: pl.DataFrame) -> list[BaseModel]:
+    return []
+
+
+async def serialize_table(
+    table_name: str, data: pd.DataFrame | pl.DataFrame, backend: DataFrameBackend = DataFrameBackend.PANDAS
+) -> list[BaseModel]:
+    if isinstance(data, pd.DataFrame):
+        return await _serialize_table_pandas(table_name=table_name, data=data)
+
+    return await _serialize_table_polars(table_name=table_name, data=data)
